5839889a317647e84d3e4d10cef7e5ffb8f6c040
Tosuke <tasukeprg@gmail.com>
2017-07-18 00:10:23 +0900

    :wrench: add markdown support

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require("gulp");
 const browserSync = require("browser-sync");
 const pug = require("gulp-pug");
 const less = require("gulp-less");
+const layout = require("gulp-layout");
+const markdown = require("gulp-markdown")
 
 const pugOptions = {
   pretty: false
@@ -9,10 +11,6 @@ const pugOptions = {
 
 const lessOptions = {}
 
-gulp.task("build", [
-  "build:pug",
-  "build:less"
-]);
 
 gulp.task("watch", () => {
   browserSync({
@@ -20,13 +18,23 @@ gulp.task("watch", () => {
       baseDir: "./dist"
     }
   })
-  gulp.watch(["src/**/*.pug", "src/**/*.less"], ["build", "reload"]);
+  gulp.watch([
+    "src/**/*.pug",
+    "src/**/*.less",
+    "src/**/*.md"
+  ], ["build", "reload"]);
 });
 
 gulp.task("reload", ["build"], () => {
   console.log("build complete")
   browserSync.reload();
 });
+
+gulp.task("build", [
+  "build:pug",
+  "build:less",
+  "build:markdown"
+]);
 
 gulp.task(
   "build:pug",
@@ -39,5 +47,15 @@ gulp.task(
   "build:less",
   () => gulp.src(["src/**/[^_]*.less"])
         .pipe(less(lessOptions))
+        .pipe(gulp.dest("dist/"))
+);
+
+gulp.task(
+  "build:markdown",
+  () => gulp.src(["src/**/*.md"])
+        .pipe(markdown())
+        .pipe(layout({
+          layout: "src/_md-template.pug",
+        }))
         .pipe(gulp.dest("dist/"))
 );
